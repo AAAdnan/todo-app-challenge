c9f6c57bb32a686908c46d9583367faf874b0e6e
Adnan Ahmed <adnan.ahmed01@gmail.com>
2023-05-13 19:10:59 +0100

    added styled components css to TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,37 @@
+import styled from 'styled-components';
 import { useContext } from 'react';
 import AddTodo from './AddTodo';
 import { TodoContext } from "../context/todo";
 
-
-
-export default function TodoList() {
-
+const TodoList = () => {
     const { state } = useContext(TodoContext);
-
+  
     console.log(state)
+  
+    return (
+      <Layout>
+        <div>
+          <Heading>TodoList</Heading>
+          <AddTodo />
+        </div>
+      </Layout>
+    );
+  };
+  
+export default TodoList;
 
-  return (
-    <>
-    <h1>TodoList</h1>
-    <AddTodo />
-    </>
-  )
-}
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #293241;
+`;
+
+const Heading = styled.h1`
+  font-size: 3rem;
+  color: #98C1D9;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
